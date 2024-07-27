8f01a194b57127231c3930527e04c55923331190
cuongdev180503 <cuotie1852003@gmail.com>
2024-07-27 17:06:34 +0700

    thu commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ const App = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World with Cuongdev</p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
-  );
+  return div;
+
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>Hello World with Cuongdev</p>
+  //     <div>Count = {count}</div>
+  //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+  //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+  //   </header>
+  // </div>
 };
 
 export default App;
